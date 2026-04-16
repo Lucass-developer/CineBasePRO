8f6e28e7d433828ac325d8158da8f5ab532ae2aa
Lucass <lsilva.kingone@gmail.com>
2026-04-16 10:27:30 -0300

    Iniciando Repositorio

---
+++ CineBasePRO/src/main/java/com/lucassdev/CineBasePRO/CineBaseProApplication.java
@@ -0,0 +1,13 @@
+package com.lucassdev.CineBasePRO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineBaseProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineBaseProApplication.class, args);
+	}
+
+}
---
+++ CineBasePRO/src/test/java/com/lucassdev/CineBasePRO/CineBaseProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucassdev.CineBasePRO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineBaseProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
